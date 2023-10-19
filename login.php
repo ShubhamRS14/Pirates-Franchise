411e44483872b877ea76b3a5a7bddf5c6c528b47
Jack Sparrow <101544581+ShubhamRS14@users.noreply.github.com>
2023-10-19 18:30:07 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,122 +1,28 @@
-<?php 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "test_db3"; 
 
-session_start();
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-	include("connect.php");
-	include("function.php");
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		//something was posted
-		$username = $_POST['username'];
-		$password = $_POST['password']; 
-		echo "$username,$password";
+$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = $conn->query($sql);
 
-		if(!empty($username) && !empty($password) && !is_numeric($username))
-		{
+if ($result->num_rows > 0) {
+  echo "Login Successfull!! <a href= 'gallery.html'>Click Here</a>";
+} else {
+  echo "Invalid Username or Password!! <a href='index2.html'>Register here</a>";
+}
 
-			//read from database
-			$query = "select * from jack where username = '$username' limit 1";
-			$result = mysqli_query($conn, $query);
-
-			if($result)
-			{
-				if($result && mysqli_num_rows($result) > 0)
-				{
-
-					$user_data = mysqli_fetch_assoc($result);
-					echo"$user_data";
-					
-					if($user_data['password'] === $password)
-					{
-
-						$_SESSION['username'] = $user_data['username'];
-						header("Location: G1.php");
-						die;
-					}
-				}
-			}
-			
-			echo "wrong username or password!";
-		}else
-		{
-			echo "wrong username or password!";
-		}
-	}
-
+$conn->close();
 ?>
 
 
-<html>
-    <head>
-        <title>Login-form</title>
-		<header>
-		<style>
-		header{
-   background-image: url(IMG1.jpg);
-	background-size:cover;
-	height: 100vh;
-	background-position: center;	
-	margin:auto;
-	padding:auto;
-	
-}
-			h1{
-		color:orange;
-		font-family: verdana;
-		font-size: 300%;
-	}
-	label{
-		color:yellow;
-		font-family:consolas;
-		font-size: 150%;
-		border: 5px  ;
-	}
-	.btn{
-		background-color: #f44336;
-		color:indigo;
-		font-family:algerian;
-	font-size: 150%;
-	cursor: pointer;
-	}
-	.password{
-		color:red;
-		border: 1px solid red;
-		padding: 0px;
-		margin: 0px;
-	}
-	a{
-		color:blue;
-		font-family: consolas;
-		border: 5px;
-	}
-	.form-group{
-		color:violet;
-		font-family: serif;
-		font-size: 100%;
-		padding:1px;
-		margin:1px;
-		background: border-box;
-		outline: none;
-	}
-		</style>
-    </head>
-    <body>
-
-        <div class="input-group">
-            <form method="POST"  class="input">
-                <h1 class="login" align= "center">Login Form</h1><br><br>
-                <label>Username:-</label>
-                <input type="text" name="username" class="form-group" required><br><br>
-                <label>Password:-</label>
-                <input type="Password" name="password" class="form-group" required>
-                <br><br>
-                <input type="submit" name="login" value="Login" class="btn" ><br>
-                <label>Don't have an account..?</label>
-                <a href="register.php" class="text">Register here</a>
-            </form>
-        </div>  	       
-    </body>
-	</header>
-</html>
--- register.php
+++ register.php
@@ -1,113 +1,26 @@
-<html>
-<head>
-<title> </title>
-</head>
-<header>
-<style>
-*{
-	margin:0px;
-	padding:0px;
+<?php
+$servername = "localhost";
+$username = "root";  // Replace with your database username
+$password = "";  // Replace with your database password
+$dbname = "test_db3";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-	
 
-header{
-   background-image: url(Logo1.jpg);
-	background-size:cover;
-	height: 100vh;
-	background-position: center;	
-	
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Registered successfully! <a href='login.html'>Back to Login</a>";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
-	h1{
-		color:gold;
-		font-family: verdana;
-		font-size: 300%;
-	}
-	label{
-		color:silver;
-		font-family:consolas;
-		font-size: 150%;
-		border: 5px solid powdergreen;
-	}
-	.btn{
-		color:indigo;
-		font-family:algerian;
-	font-size: 150%;
-	background: border:box;
-	cursor:pointer;
-	overflow: hidden;
-  text-overflow: clip;
-	}
-	.password{
-		color:red;
-		border: 1px solid red;
-		padding: 0px;
-		margin: 0px;
-	}
-	a{
-		color: white;
-		font-family: castellar;
-	}
-	.form-group{
-		color:yellow;
-		font-family: Times new roman;
-		font-size: 100%;
-		padding:5px;
-		margin:5px;
-		background: border-box;
-		outline: none;
-	}
-	.group{
-		background: border-box;
-	}
-	
-</style>
 
-<form  class="group" action= "register.php" method= "POST">
-<h1 align= "center"> Registeration Form </h1>
-<label>Username:-</label>
-                <input type="text" name="username" class="form-group" required><br><br>
-                <label>Email:-</label>
-                <input type="email" name="email" class="form-group" required><br><br>
-                <label>Password:-</label>
-                <input type="Password" name="password" class="form-group" required>
-<br><br>
-                <input type="submit" name="registration" class="btn" value="Register" ><br></br> <br> </br>
-                <label>Already have an account..?</label>
-                <a href="login.php" class="text">Login here</a>
-            </form>
-
-<?php 
-session_start();
-include ("connect.php");
-	
-
-
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		//something was posted
-		$username = $_POST['username'];
-        $email= $_POST['email'];
-		$password = $_POST['password'];
-
-
-		if(!empty($username) && !empty($password) && !empty($email) && !is_numeric($username))
-		{
-
-			//save to database
-			
-			$query = "insert into jack (username,email,password) values ('$username','$email','$password')";
-
-			mysqli_query($conn, $query);
-
-			header("Location: login.php");
-			die;
-		}else
-		{
-			echo "Please enter some valid information!";
-		}
-	}
-	
+$conn->close();
 ?>
-</header>
-</html>
-
